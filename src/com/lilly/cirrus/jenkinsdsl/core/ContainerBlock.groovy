259ff8a1d772af7ec8290133d423bc5e8ded0f41
vksingh770 <singh_vinay@network.lilly.com>
2021-09-21 14:28:23 -0500

    Adding a 30 sec test timout to fail the build

--- src/com/lilly/cirrus/jenkinsdsl/core/ContainerBlock.groovy
+++ src/com/lilly/cirrus/jenkinsdsl/core/ContainerBlock.groovy
@@ -32,6 +32,8 @@ abstract class ContainerBlock extends Block {
     }
     withJenkins {
         echo "Adding comments in the preperation of timeout for the pipeline"
+        sh "sleep 60"
+        error "Failing the build due to timeout"
     }
 
     openShiftClient.configureCredentials(namespaceToRegistryToCredentialId)
